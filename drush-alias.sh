367afd7a46cb318202c5e5c38bda4113298fc88b
kdb05506 <kdb05506@creighton.edu>
2018-07-06 14:24:35 -0500

    create shell script for generating ACSF site alias files

---
+++ drush-alias.sh
@@ -0,0 +1,29 @@
+#!/bin/sh
+# Generate ACSF site aliases for all three environments
+echo 'Please enter the site name: '
+read site_name
+
+echo "dev:" > "drush/sites/$site_name.site.yml"
+echo "  root: /var/www/html/creighton.01dev/docroot" >> "drush/sites/$site_name.site.yml"
+echo "  uri: $site_name.dev-creighton.acsitefactory.com" >> "drush/sites/$site_name.site.yml"
+echo "  host: staging-1749.enterprise-g1.hosting.acquia.com" >> "drush/sites/$site_name.site.yml"
+echo "  options: {  }" >> "drush/sites/$site_name.site.yml"
+echo "  paths: { dump-dir: /mnt/tmp }" >> "drush/sites/$site_name.site.yml"
+echo "  user: creighton.01dev" >> "drush/sites/$site_name.site.yml"
+echo "  ssh: { options: '-p 22' }" >> "drush/sites/$site_name.site.yml"
+echo "test:" >> "drush/sites/$site_name.site.yml"
+echo "  root: /var/www/html/creighton.01test/docroot" >> "drush/sites/$site_name.site.yml"
+echo "  uri: $site_name.test-creighton.acsitefactory.com" >> "drush/sites/$site_name.site.yml"
+echo "  host: staging-1749.enterprise-g1.hosting.acquia.com" >> "drush/sites/$site_name.site.yml"
+echo "  options: {  }" >> "drush/sites/$site_name.site.yml"
+echo "  paths: { dump-dir: /mnt/tmp }" >> "drush/sites/$site_name.site.yml"
+echo "  user: creighton.01test" >> "drush/sites/$site_name.site.yml"
+echo "  ssh: { options: '-p 22' }" >> "drush/sites/$site_name.site.yml"
+echo "live:" >> "drush/sites/$site_name.site.yml"
+echo "  root: /var/www/html/creighton.01live/docroot" >> "drush/sites/$site_name.site.yml"
+echo "  uri: $site_name.creighton.acsitefactory.com" >> "drush/sites/$site_name.site.yml"
+echo "  host: web-1747.enterprise-g1.hosting.acquia.com" >> "drush/sites/$site_name.site.yml"
+echo "  options: {  }" >> "drush/sites/$site_name.site.yml"
+echo "  paths: { dump-dir: /mnt/tmp }" >> "drush/sites/$site_name.site.yml"
+echo "  user: creighton.01live" >> "drush/sites/$site_name.site.yml"
+echo "  ssh: { options: '-p 22' }" >> "drush/sites/$site_name.site.yml"
