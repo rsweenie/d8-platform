bb6af031ba67bb79de91feadfae477b73f0f4255
kdb05506 <kdb05506@creighton.edu>
2018-07-17 08:33:58 -0500

    update db-update hook to run config import on code update

--- factory-hooks/db-update/db-update.sh
+++ factory-hooks/db-update/db-update.sh
@@ -31,6 +31,6 @@ echo "$site.$env: Running BLT deploy tasks on $uri domain in $env environment on
 IFS='.' read -a name <<< "${uri}"
 
 $blt drupal:update --environment=$env --site=${name[0]} --define drush.uri=$domain --verbose --yes
-
-echo "Testing db-update hook"
-curl -X POST -H 'Content-type: application/json' --data '{"text":"Code+db updated on $domain"}' https://hooks.slack.com/services/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
+echo "Running config import on $site.$env"
+drush @"$site.$env" config:import vcs -y
+curl -X POST -H "Content-type: application/json" --data "{\"text\":\"Code updated on $site.$env\"}" https://hooks.slack.com/services/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
