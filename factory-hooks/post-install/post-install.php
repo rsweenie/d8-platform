bb1a87c8c1db2d644e18f7ac60d909755626fee4
kdb05506 <kdb05506@creighton.edu>
2018-07-17 08:38:24 -0500

    remove unused code previously added to factory hooks

--- factory-hooks/post-install/post-install.php
+++ factory-hooks/post-install/post-install.php
@@ -25,9 +25,3 @@ $domain_fragments = explode('.', $_SERVER['HTTP_HOST']);
 $site_name = array_shift($domain_fragments);
 
 exec("/mnt/www/html/$site.$env/vendor/acquia/blt/bin/blt drupal:update --environment=$env --site=$site_name --define drush.uri=$domain --verbose --yes");
-
-exec("curl -X POST -H Content-type: application/json --data {'text':'Hello, World! Site created on ACSF. '} https://hooks.slack.com/services/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");
-// 1. check if alias exists
-// 1a. if yes, do nothing
-// 1b. if no, execute shell script
-// 2. post result to slack
