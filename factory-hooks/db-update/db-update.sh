e3e30a6182c7289e5b74a47a3a1a596e4685e8a8
Kikki <kristinbeltz@creighton.edu>
2018-07-20 15:23:35 -0500

    update script to echo command to see if it's running properly

--- factory-hooks/db-update/db-update.sh
+++ factory-hooks/db-update/db-update.sh
@@ -49,6 +49,6 @@ drush @"$siteName.$subEnv" config:import vcs -y
 
 # Scrubbing functions to fix login errors
 drush @"$siteName.$subEnv" acsf-duplication-scrub-batch "$siteName" "$site"
-
+echo drush @"$siteName.$subEnv" acsf-duplication-scrub-batch "$siteName" "$site"
 # Push a notification to the #edw channel on slack
 curl -X POST -H "Content-type: application/json" --data "{\"text\":\"Code updated on $siteName.$subEnv\"}" https://hooks.slack.com/services/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
