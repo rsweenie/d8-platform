a53af47f62845040d18d46a0a07f97dd565b3ccf
Kikki <kristinbeltz@creighton.edu>
2018-07-20 08:25:40 -0500

    acsf login fix

--- factory-hooks/db-update/db-update.sh
+++ factory-hooks/db-update/db-update.sh
@@ -44,5 +44,8 @@ subEnv="${env#01}"
 # Config import any changes just pushed to the code base
 drush @"$siteName.$subEnv" config:import vcs -y
 
+# Scrubbing functions to fix login errors
+drush @"$siteName.$subEnv" acsf-duplication-scrub-batch "$siteName" "$site"
+
 # Push a notification to the #edw channel on slack
 curl -X POST -H "Content-type: application/json" --data "{\"text\":\"Code updated on $siteName.$subEnv\"}" https://hooks.slack.com/services/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
