99b42af4f2e8ffb88164685e5fdbfd0d58f2c0af
Kikki <kristinbeltz@creighton.edu>
2018-08-01 11:33:14 -0500

    complete rsync script

--- file-sync.sh
+++ file-sync.sh
@@ -1,14 +1,25 @@
 #!/bin/bash
 # Copy files down from selected Acquia site
 
-echo 'Please enter the alias of the site you would like to copy files from: '
-read site_alias
+echo 'Please enter the name of the site you would like to copy files from: '
+read site_name
+echo 'Please select the environment to copy files from: '
+options=('dev' 'test' 'live')
+declare env
+select option in "${options[@]}"
+do
+    case $option in 
+        'dev'|'test'|'live')
+            env=$option
+            break
+            ;;
+        *)
+            echo "$REPLY is not a valid option. Please try again: "
+            ;;
+    esac
+done
 
-# Gives @ to site alias if it's omitted
-if !(echo $site_alias | grep -q "@");
-then
-    site_alias="@$site_alias"
-fi;
+site_alias="@$site_name.$env"
 
 # Finds public file path for selected Acquia site
 public_files="Files, Public : "
@@ -19,9 +30,12 @@ do
     then
         prefix=$(echo "$public_files" | sed 's/ //g')
         new_line=$(echo "$line" | sed 's/ //g')
-        file_path=${new_line#$prefix}
+        file_path=$(echo "${new_line#$prefix}" | tr '\r' ' ' | sed 's/ //g')
     fi;
 done < <(drush $site_alias core:status)
 
+# If alias is incorrect, the return will be
+# [preflight] The alias @$site_alias could not be found.
+
 # Copies images with image styles from the selected Acquia site to the local environment
-drush rsync "$site_alias":/var/www/html/creighton.01live/docroot/sites/g/files/indaly711/files/ @self:/var/www/creighton/docroot/sites/default/files/ -y
+eval drush --debug --exclude-paths /acsf_tmp:/bootstrap:/media-icons:/xmlsitemap:/.htaccess "$site_alias:/var/www/html/creighton.01$env/docroot/$file_path @self:/var/www/creighton/docroot/sites/default/ -y"
