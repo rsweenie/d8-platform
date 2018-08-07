4eb7110f107479bb8cca6611613482a6cdd25b4b
Kikki <kristinbeltz@creighton.edu>
2018-08-07 16:04:47 -0500

    update sso config script"

--- sso-config.sh
+++ sso-config.sh
@@ -3,24 +3,23 @@
 
 echo 'Please enter the name of the production site on which to configure SSO: '
 read site_name
-
 site_alias="@$site_name.01live"
-
-# Finds public file path for selected Acquia site
-public_files="Files, Public : "
-declare file_path
+# Finds site id for selected Acquia site
+site_path="Site path"
 while read line
 do
-    if echo $line | grep -q "$public_files";
+    if echo $line | grep -q "$site_path";
     then
-        prefix=$(echo "$public_files" | sed 's/ //g')
+        echo "LINE: $line"
+        # prefix=$(echo "$site_path sites/g/files/indaly" | sed 's/ //g')
         new_line=$(echo "$line" | sed 's/ //g')
-        file_path=$(echo "${new_line#$prefix}" | tr '\r' ' ' | sed 's/ //g')
+        site_id=$(echo "${new_line#'Site path        : sites/g/files/indaly'}" | tr '\r' ' ' | sed 's/ //g')
+        echo "Site ID: $site_id"
     fi;
 done < <(drush $site_alias core:status)
 
 # If alias is incorrect, the return will be
 # [preflight] The alias @$site_alias could not be found.
 
-# Copies images with image styles from the selected Acquia site to the local environment
-eval drush rsync --debug --exclude-paths /acsf_tmp:/bootstrap:/media-icons:/xmlsitemap:/.htaccess "$site_alias:/var/www/html/creighton.$env/docroot/$file_path @self:/var/www/creighton/docroot/sites/default/ -y"
+# Sets sp_entity_id for ACSF SSO into the selected site
+# eval drush @site_alias cset samlauth.authentication sp_entity_id "urn:acquia:acsf:saml:sp:creighton:01live:$site_id"
