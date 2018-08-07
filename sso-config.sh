9a4012c533c43f090da8f7a7a8a09280e0ca9919
Kikki <kristinbeltz@creighton.edu>
2018-08-07 13:38:00 -0500

    start sso config script, modify drush alias script to more accurately reflect the aliases generated by blt.

---
+++ sso-config.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+# Copy files down from selected Acquia site
+
+echo 'Please enter the name of the production site on which to configure SSO: '
+read site_name
+
+site_alias="@$site_name.01live"
+
+# Finds public file path for selected Acquia site
+public_files="Files, Public : "
+declare file_path
+while read line
+do
+    if echo $line | grep -q "$public_files";
+    then
+        prefix=$(echo "$public_files" | sed 's/ //g')
+        new_line=$(echo "$line" | sed 's/ //g')
+        file_path=$(echo "${new_line#$prefix}" | tr '\r' ' ' | sed 's/ //g')
+    fi;
+done < <(drush $site_alias core:status)
+
+# If alias is incorrect, the return will be
+# [preflight] The alias @$site_alias could not be found.
+
+# Copies images with image styles from the selected Acquia site to the local environment
+eval drush rsync --debug --exclude-paths /acsf_tmp:/bootstrap:/media-icons:/xmlsitemap:/.htaccess "$site_alias:/var/www/html/creighton.$env/docroot/$file_path @self:/var/www/creighton/docroot/sites/default/ -y"
