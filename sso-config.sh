ac9aebb898755136efe058df295b3c5fed4d5197
Kikki <kristinbeltz@creighton.edu>
2018-10-05 11:51:20 -0500

    add samlauth.authentication to config split. add social media links block to config ignore. modify sso-config script to take all environments into account.

--- sso-config.sh
+++ sso-config.sh
@@ -3,10 +3,19 @@
 
 # takes site name as an argument
 # $1 = site name
+# $2 = environment
 
-site_alias="@$1.01live"
+# site_alias="@$1.$2"
 # Finds site id for selected Acquia site
 site_path="Site path"
+uri_prefix=""
+
+if [ "$2" == "01dev" ] || [ "$2" == "01test" ];
+then
+    env_prefix="01"
+    uri_prefix="${2#$env_prefix}-"
+fi;
+
 while read line
 do
     if echo $line | grep -q "$site_path";
@@ -15,11 +24,11 @@ do
         prefix=$(echo "$site_path:sites/g/files/indaly" | sed 's/ //g')
         site_id=$(echo "${new_line#$prefix}" | tr '\r' ' ' | sed 's/ //g')
     fi;
-done < <(/mnt/www/html/creighton01live/vendor/bin/drush @self core:status --uri=$1.creighton.acsitefactory.com --no-interaction -v --ansi)
+done < <(/mnt/www/html/creighton"$2"/vendor/bin/drush @self core:status --uri="$1"."$uri_prefix"creighton.acsitefactory.com --no-interaction -v --ansi)
 
-# If alias is incorrect, the return will be
-# [preflight] The alias @$site_alias could not be found.
+# eval "/mnt/www/html/creighton"$2"/vendor/bin/drush @self cim -y --uri=$1.$envcreighton.acsitefactory.com --no-interaction -v --ansi"
+echo "/mnt/www/html/creighton"$2"/vendor/bin/drush @self cim -y --uri="$1"."$uri_prefix"creighton.acsitefactory.com --no-interaction -v --ansi"
 
-eval /mnt/www/html/creighton01live/vendor/bin/drush @self cim -y --uri=$1.creighton.acsitefactory.com --no-interaction -v --ansi
 # Sets sp_entity_id for ACSF SSO into the selected site
-eval /mnt/www/html/creighton01live/vendor/bin/drush @self cset samlauth.authentication sp_entity_id "urn:acquia:acsf:saml:sp:creighton:01live:$site_id" -y --uri=$1.creighton.acsitefactory.com --no-interaction -v --ansi
+# eval "/mnt/www/html/creighton"$2"/vendor/bin/drush @self cset samlauth.authentication sp_entity_id urn:acquia:acsf:saml:sp:creighton:"$env":$site_id -y --uri="$1"."$env"creighton.acsitefactory.com --no-interaction -v --ansi"
+echo "/mnt/www/html/creighton"$2"/vendor/bin/drush @self cset samlauth.authentication sp_entity_id urn:acquia:acsf:saml:sp:creighton:"$2":"$site_id" -y --uri="$1"."$uri_prefix"creighton.acsitefactory.com --no-interaction -v --ansi"
